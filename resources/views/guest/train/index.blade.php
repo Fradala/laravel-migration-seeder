e71e2fbbbdbdf23ea7686cbe59e6f6f374c74933
Francesco.dalascio <francesco.dalascio.2002@gmail.com>
2024-02-05 16:31:19 +0100

    inseriti in pagina le info dei treni

--- resources/views/guest/train/index.blade.php
+++ resources/views/guest/train/index.blade.php
@@ -2,7 +2,21 @@
     <h2>
         treni
     </h2>
+    <ul>
+        @forelse ($trains as $train )
+            <li>
+                {{ $train->name }} : {{ $train->stazione_partenza }} - {{ $train->stazione_arrivo }}
+                {{ $train->orario_partenza }} - {{ $train->orario_arrivo }}
+                {{ $train->in_orario }} - {{ $train->cancellato }}
+            </li>
+        
+        @empty
+        
+        @endforelse
+    </ul>
+    
 
+ 
 @section('main-content')
 
 @endsection
